0a154e81753baa62abad14d55578052e232159f1
DavidFerrs <fernandes.-.david@hotmail.com>
2025-02-19 18:54:08 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by David Ferreira on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
